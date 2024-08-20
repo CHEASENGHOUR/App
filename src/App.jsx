296e8205e3d39a5b03062bd95bb4c5b1039828de
CHEASENGHOUR <cheasenghour789@gmail.com>
2024-08-20 22:06:03 +0700

    Nice

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import List from "./components/list";
+function App(){
+  const h = "Hi"
+  return(
+    <>
+      <h1>Hello JavaScripts</h1>
+      <h1>{h}</h1>
+      <List />
+    </>
+  )
+}
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,10 @@
+function List(){
+    return(
+        <>
+        	<h1>Hi , I am a list</h1>
+            <img src="../assets/code.png" alt=""/>
+            <a  href="#/"></a>
+        </>
+    )
+}
+export default List;
